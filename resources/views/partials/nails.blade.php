481b3330da716777df71276025bbd965f5efba8d
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-07-24 15:50:15 +0200

    Update nails.blade.php

--- resources/views/partials/nails.blade.php
+++ resources/views/partials/nails.blade.php
@@ -4,6 +4,7 @@
         <h2 class="u-align-center u-text u-text-default u-text-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="0"> Körmös szolgáltatások </h2>
         <p class="u-align-center u-text u-text-2" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="250">21 naptól vagy a szett előkészítését követően 30%-osnál hiányosabb pilla esetén építési ára kerül felszámításra!<br>Az árak 2024.03.01-től érvényesek!
         </p>
+        @foreach($nailOpportunities as $nailOpportunitie)
         <div class="u-expanded-width-xs u-list u-list-1">
           <div class="u-repeater u-repeater-1">
             <div class="u-container-style u-list-item u-repeater-item u-white u-list-item-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
@@ -25,7 +26,7 @@
             <div class="u-container-style u-list-item u-repeater-item u-white u-list-item-2" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
               <div class="u-container-layout u-similar-container u-container-layout-2">
                 <div alt="" class="u-image u-image-circle u-image-2"  style="background-image: url(' {{ asset("img/skincare-beauty-female-feet-with-camomile-s-flower_186202-728.jpg") }}')" ;></div>
-                <h4 class="u-align-center u-custom-font u-font-lobster u-text u-text-palette-4-base u-text-14">Géllakk</h4>
+                <h4 class="u-align-center u-custom-font u-font-lobster u-text u-text-palette-4-base u-text-14">{{$nailOpportunitie->type}}</h4>
                 <p class="u-align-left u-text u-text-palette-4-base u-text-15">6.000 Ft. </p>
                 <p class="u-align-center u-text u-text-palette-4-base u-text-16">S</p>
                 <p class="u-align-center u-text u-text-palette-4-base u-text-17">M </p>
@@ -45,12 +46,12 @@
             <div class="u-container-style u-list-item u-repeater-item u-white u-list-item-3" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
               <div class="u-container-layout u-similar-container u-container-layout-3">
                 <div alt="" class="u-image u-image-circle u-image-3"  style="background-image: url(' {{ asset("img/uyj-min.jpg") }}')" ;></div>
-                <h3 class="u-custom-font u-font-lobster u-text u-text-default u-text-palette-2-base u-text-25">Géllak </h3>
+                <h3 class="u-custom-font u-font-lobster u-text u-text-default u-text-palette-2-base u-text-25">{{$nailOpportunitie->type}}</h3>
                 <p class="u-align-left u-text u-text-palette-2-base u-text-26">4.800 Ft.</p>
                 <p class="u-text u-text-default u-text-palette-2-base u-text-27">Gél​lak</p>
                 <p class="u-align-center u-text u-text-palette-2-base u-text-28">Prémium erősített géllakk</p>
                 <p class="u-align-left u-text u-text-palette-2-base u-text-29">4.800 Ft.</p>
-                <h3 class="u-align-left u-custom-font u-font-lobster u-text u-text-default u-text-palette-2-base u-text-30">Díszítés</h3>
+                <h3 class="u-align-left u-custom-font u-font-lobster u-text u-text-default u-text-palette-2-base u-text-30">{{$nailOpportunitie->type}}</h3>
                 <p class="u-align-left u-text u-text-palette-2-base u-text-31">1.000 Ft./ujj</p>
                 <p class="u-align-left u-text u-text-palette-2-base u-text-32">500 Ft./db</p>
                 <p class="u-align-left u-text u-text-palette-2-base u-text-33">2.000 Ft./szett</p>
@@ -80,4 +81,8 @@
           </div>
         </div>
       </div>
+      @endforeach
+      <a href="#" class="fixedbutton">
+  <i class="bi bi-arrow-up-circle-fill " id="fixedbutton"></i>
+</a>
     </section>
