faba3d71160ec58afc357bdbe3c8675302dd0030
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-07-25 12:33:42 +0200

    Create NailType.php

---
+++ app/Models/NailType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class NailType extends Model
+{
+    protected $table = 'nail_type';
+    protected $fillable = [
+        'type',
+    ];
+
+    public function Opps() : BelongsToMany
+    {
+        return $this->belongsToMany(NailOpportunities::class, 'nails_groupment', 'type_id', 'opp_id');
+    }
+
+    public function Prices() : BelongsToMany
+    {
+        return $this->belongsToMany(Price::class, 'nails_groupment', 'type_id', 'price_id');
+    }
+}
+
