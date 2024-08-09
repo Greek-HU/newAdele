2b0dcba47252ffe5cac0abaf08311dc2728f3f91
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-08-09 12:54:39 +0200

    Update AdeleDB.sql

--- DB/AdeleDB.sql
+++ DB/AdeleDB.sql
@@ -1,193 +1,114 @@
--- --------------------------------------------------------
--- Hoszt:                        127.0.0.1
--- Szerver verzió:               8.0.32 - MySQL Community Server - GPL
--- Szerver OS:                   Win64
--- HeidiSQL Verzió:              12.3.0.6589
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Adatbázis struktúra mentése a adelenail.
-CREATE DATABASE IF NOT EXISTS `adelenail` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `adelenail`;
-
--- Struktúra mentése tábla adelenail. comment
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `comment` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `comment_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. failed_jobs
-CREATE TABLE IF NOT EXISTS `failed_jobs` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. migrations
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. nails_groupment
-CREATE TABLE IF NOT EXISTS `nails_groupment` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `type_id` bigint unsigned NOT NULL,
-  `size_id` bigint unsigned NOT NULL,
-  `price_id` bigint unsigned NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `nails_groupment_type_id_foreign` (`type_id`),
-  KEY `nails_groupment_size_id_foreign` (`size_id`),
-  KEY `nails_groupment_price_id_foreign` (`price_id`),
-  CONSTRAINT `nails_groupment_price_id_foreign` FOREIGN KEY (`price_id`) REFERENCES `price` (`id`),
-  CONSTRAINT `nails_groupment_size_id_foreign` FOREIGN KEY (`size_id`) REFERENCES `nail_size` (`id`),
-  CONSTRAINT `nails_groupment_type_id_foreign` FOREIGN KEY (`type_id`) REFERENCES `nail_opportunities` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. nail_opportunities
-CREATE TABLE IF NOT EXISTS `nail_opportunities` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `options` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. nail_size
-CREATE TABLE IF NOT EXISTS `nail_size` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `size_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. password_reset_tokens
-CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. personal_access_tokens
-CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint unsigned NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `expires_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. pictures
-CREATE TABLE IF NOT EXISTS `pictures` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `imgPlace` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `pictures_imgplace_unique` (`imgPlace`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. price
-CREATE TABLE IF NOT EXISTS `price` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `amount` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. size_price
-CREATE TABLE IF NOT EXISTS `size_price` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `size_id` bigint unsigned NOT NULL,
-  `price_id` bigint unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `size_price_size_id_foreign` (`size_id`),
-  KEY `size_price_price_id_foreign` (`price_id`),
-  CONSTRAINT `size_price_price_id_foreign` FOREIGN KEY (`price_id`) REFERENCES `price` (`id`),
-  CONSTRAINT `size_price_size_id_foreign` FOREIGN KEY (`size_id`) REFERENCES `nail_size` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla adelenail. users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phoneNum` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Az adatok exportálása nem lett kiválasztva.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+UpdatecheckLastrun<|||>1<|||>2024-08-09 10:27:24
+LastUsageStatisticCall<|||>1<|||>2024-07-26 17:27:55
+LastSessions<|||>1<|||>LocalDB
+LastActiveSession<|||>1<|||>LocalDB
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+ColWidths_frmTableEditor.listColumns<|||>1<|||>40,100,154,90,60,65,50,117,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>641,100,80,120,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>607,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,403
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+querymemoheight<|||>3<|||>300
+MainWinMaximized<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,316
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,316,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,186
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,256
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+SessionManager_WindowLeft<|||>3<|||>33
+SessionManager_WindowTop<|||>3<|||>27
+DateTimeEditor_CursorPos_Type4<|||>3<|||>14
+MainWinLeft<|||>3<|||>2107
+MainWinWidth<|||>3<|||>1426
+MainWinHeight<|||>3<|||>805
+FileDialogEncoding_MainForm<|||>3<|||>0
+CreateDbCollation<|||>1<|||>utf8mb4_general_ci
+ColWidths_frmRoutineEditor.listParameters<|||>1<|||>50,385,90,70
+ColsVisible_frmRoutineEditor.listParameters<|||>1<|||>0,1,2,3
+ColPositions_frmRoutineEditor.listParameters<|||>1<|||>0,1,2,3
+ColSort_frmRoutineEditor.listParameters<|||>1<|||>-1,0
+MainWinTop<|||>3<|||>110
+SQLFile0<|||>1<|||>D:\Laravel\newAdele\DB\AdeleDB.sql
+SQLFile1<|||>1<|||>C:\Users\gregu\Downloads\sakila-db\sakila-data.sql
+SQLFile2<|||>1<|||>C:\Users\gregu\Downloads\sakila-db\sakila-schema.sql
+Servers\LocalDB\SessionCreated<|||>1<|||>2024-07-26 17:27:58
+Servers\LocalDB\Host<|||>1<|||>127.0.0.1
+Servers\LocalDB\WindowsAuth<|||>3<|||>0
+Servers\LocalDB\CleartextPluginEnabled<|||>3<|||>0
+Servers\LocalDB\User<|||>1<|||>root
+Servers\LocalDB\Password<|||>1<|||>5
+Servers\LocalDB\LoginPrompt<|||>3<|||>0
+Servers\LocalDB\Port<|||>1<|||>3306
+Servers\LocalDB\NetType<|||>3<|||>0
+Servers\LocalDB\Compressed<|||>3<|||>0
+Servers\LocalDB\LocalTimeZone<|||>3<|||>0
+Servers\LocalDB\QueryTimeout<|||>3<|||>30
+Servers\LocalDB\KeepAlive<|||>3<|||>20
+Servers\LocalDB\FullTableStatus<|||>3<|||>1
+Servers\LocalDB\Databases<|||>1<|||>
+Servers\LocalDB\Library<|||>1<|||>libmariadb.dll
+Servers\LocalDB\Comment<|||>1<|||>
+Servers\LocalDB\StartupScriptFilename<|||>1<|||>
+Servers\LocalDB\TreeBackground<|||>3<|||>536870911
+Servers\LocalDB\SSHtunnelActive<|||>3<|||>0
+Servers\LocalDB\SshExecutable<|||>1<|||>
+Servers\LocalDB\SSHtunnelHost<|||>1<|||>
+Servers\LocalDB\SSHtunnelHostPort<|||>3<|||>22
+Servers\LocalDB\SSHtunnelUser<|||>1<|||>
+Servers\LocalDB\SSHtunnelPassword<|||>1<|||>1
+Servers\LocalDB\SSHtunnelTimeout<|||>3<|||>4
+Servers\LocalDB\SSHtunnelPrivateKey<|||>1<|||>
+Servers\LocalDB\SSHtunnelPort<|||>3<|||>3307
+Servers\LocalDB\SSL_Active<|||>3<|||>0
+Servers\LocalDB\SSL_Key<|||>1<|||>
+Servers\LocalDB\SSL_Cert<|||>1<|||>
+Servers\LocalDB\SSL_CA<|||>1<|||>
+Servers\LocalDB\SSL_Cipher<|||>1<|||>
+Servers\LocalDB\SSL_Verification<|||>3<|||>2
+Servers\LocalDB\IgnoreDatabasePattern<|||>1<|||>
+Servers\LocalDB\LogFileDdl<|||>3<|||>0
+Servers\LocalDB\LogFileDml<|||>3<|||>0
+Servers\LocalDB\LogFilePath<|||>1<|||>C:\Users\gregu\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\LocalDB\ServerVersionFull<|||>1<|||>10.4.32-MariaDB - mariadb.org binary distribution
+Servers\LocalDB\ConnectCount<|||>3<|||>15
+Servers\LocalDB\ServerVersion<|||>3<|||>100432
+Servers\LocalDB\LastConnect<|||>1<|||>2024-08-09 10:27:27
+Servers\LocalDB\lastUsedDB<|||>1<|||>adelenail
+Servers\LocalDB\adelenail|migrations\Sort<|||>1<|||>0_batch|
+Servers\LocalDB\adelenail|nail_opportunities\MemoEditorHighlighter_size<|||>1<|||>
+Servers\LocalDB\adelenail|nail_type\MemoEditorHighlighter_type<|||>1<|||>
